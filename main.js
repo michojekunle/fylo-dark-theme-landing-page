bbb92a786810ea16ae702b980db8579da8dea8ba
AMD <michojekunle1@gmail.com>
2022-09-13 17:42:46 +0100

    added form validation

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const emailInput = document.querySelector('input');
 const form  = document.querySelector('.form');
-const errorMessage = document.querySelector('.form-error');
+const errorMessage = document.querySelector('#form-error');
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+form.addEventListener('submit', (e) => {
+    if (emailInput.value === ''){
+        e.preventDefault();
+        errorMessage.innerText = 'Email is required, Please enter your Email!';
+    } else if(isEmailValid(emailInput.value.trim())){
+
+    } else {
+        e.preventDefault();
+        errorMessage.innerText = 'Invalid Email, Please type in a valid email!';
+    }
+})
 
